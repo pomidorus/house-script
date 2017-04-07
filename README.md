3cee7e788193463fdd079d5686d303c9bd113567
Andrey Seleznov <andrewaka39@gmail.com>
2017-04-07 14:25:43 +0100

    post hpi data on each row of csv file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # House Script
 Main purpose of this script is provide data for house api cloud application
 `./main.rb`
+
+Right now it is one request per one csv row - so it is very slow. But can be fastened by implementing bulk request
--- main.rb
+++ main.rb
@@ -1,7 +1,25 @@
 require 'csv'
+require 'net/http'
+require 'uri'
+require 'json'
 
-CSV.foreach("data/hpi_sample.csv", headers: true) do |row|
- p row['Date']
- p row['RegionName']
- p row['Index']
+HPI_DATA_FILE = 'data/hpi_sample.csv'
+HOUSE_API_URI = 'http://house-api-test.herokuapp.com/hpi'
+
+uri = URI.parse(HOUSE_API_URI)
+header = {'Content-Type': 'application/json'}
+http = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Post.new(uri.request_uri, header)
+
+CSV.foreach(HPI_DATA_FILE, headers: true) do |row|
+  hpi = {
+    date: row['Date'],
+    region_name: row['RegionName'],
+    index: row['Index'].to_f
+  }
+
+  request.body = hpi.to_json
+  p response = http.request(request)
 end
+
+p 'Done!'
